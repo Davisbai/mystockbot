585c6f643915ef81a72d239eed8f1fbf4045c519
Davisbai <61259633+Davisbai@users.noreply.github.com>
2026-04-29 13:22:22 +0000

    Update: Fix fundamental data and UI alignment

--- PUSH.sh
+++ PUSH.sh
@@ -1,8 +1,4 @@
-# 1. 確保所有變更都已加入
+#!/bin/bash
 git add .
-
-# 2. 提交變更
-git commit -m "Fix requirements file location"
-
-# 3. 使用你的 Token 強制推送到雲端 (請把 <TOKEN> 換成你剛才產生的 ghp_...)
-git push https://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@github.com/Davisbai/mystockbot.git main
+git commit -m "Update: Fix fundamental data and UI alignment"
+git push origin main
